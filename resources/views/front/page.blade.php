1120512584e5f7b85e246e5d7fc54effaf25d106
Eihabhala.io <me@eihabhala.io>
2021-07-18 12:21:41 +0100

    add pages

--- resources/views/front/page.blade.php
+++ resources/views/front/page.blade.php
@@ -1,7 +1,9 @@
-
 @extends('layouts.front.front')
 @section('title', e($page->page_name))
 @section('content')
+<style>
+body{background:white}
+</style>
 <div class="page_content">
     <div class="container">
         <h1 class="text-center">{{$page->page_name}}</h1>
